c073ab6dbf1c7f7d2c45fc9e4cdce7381fa62b82
harish <harishkummari970@gmail.com>
2022-12-24 09:52:31 +0530

    adding files

---
+++ src/test/java/azure/pipeline/project/azuredevops/AzureDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package azure.pipeline.project.azuredevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
